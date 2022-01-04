9a665f292e9ec7dae6363427e770c7f3c0c39df8
noweder <hmnoweder@gmail.com>
2022-01-05 00:24:44 +0200

    Code optimization

--- BMI Program/bmi.py
+++ BMI Program/bmi.py
@@ -8,11 +8,11 @@ bmi = round(weight / (height ** 2))
 print(bmi)
 if bmi < 18.5:
     print(f"Your BMI is {bmi}, you are underweight.")
-elif 18.5 <= bmi < 25:
+elif bmi < 25:
     print(f"Your BMI is {bmi}, you have a normal weight.") 
-elif 25 <= bmi < 30:
+elif bmi < 30:
     print(f"Your BMI is {bmi}, you are slightly overweight.")
-elif 30 <= bmi < 35:
+elif bmi < 35:
     print(f"Your BMI is {bmi}, you are obese.")
-elif bmi >= 35:
+else:
     print(f"Your BMI is {bmi}, you are clinically obese.")
