10716262275bc798db41283ebfcaa56588518629
noweder <hmnoweder@gmail.com>
2022-01-08 00:26:34 +0200

    modified code

--- Pizza Order Practice/pizza-order-exercise.py
+++ Pizza Order Practice/pizza-order-exercise.py
@@ -1,56 +1,25 @@
-## Pizza Order
+print("Welcome to Python Pizza Deliveries!")
+size = input("What size pizza do you want? S, M, or L ")
+add_pepperoni = input("Do you want pepperoni? Y or N ")
+extra_cheese = input("Do you want extra cheese? Y or N ")
 
-# Instructions
+bill = 0
+if size == "S":
+    bill += 15
 
-This example shows you how to build an automatic pizza order program. 
+elif size == "M":
+    bill += 20
 
-Based on a user's order, work out their final bill. 
+elif size == "L":
+    bill += 25
 
-```
-Small Pizza: $15
-```
+if add_pepperoni == "Y":
+  if size == "S":
+    bill += 2
+  else:
+    bill += 3
 
-```
-Medium Pizza: $20
-```
+if extra_cheese == "Y":
+    bill += 1
+print(f"Your final bill is: ${bill}")
 
-```
-Large Pizza: $25
-```
-
-```
-Pepperoni for Small Pizza: +$2
-```
-
-```
-Pepperoni for Medium or Large Pizza: +$3
-```
-
-```
-Extra cheese for any size pizza: + $1
-```
-
-# Example Input
-
-```
-size = "L"
-```
-
-```
-add_pepperoni = "Y"
-```
-
-```
-extra_cheese = "N"
-```
-
-# Example Output
-
-```
-Your final bill is: $28.
-```
-
-e.g. When you hit **run**, this is what should happen:  
-
- 
-![](https://cdn.fs.teachablecdn.com/p1evEkwQxGNR4WlolIb4)
