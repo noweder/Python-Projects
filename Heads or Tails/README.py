81374d4b4bc8f79a91d375344af2167786fd479f
noweder <hmnoweder@gmail.com>
2022-01-08 23:13:14 +0200

    added heads or tails

---
+++ Heads or Tails/README.py
@@ -0,0 +1,23 @@
+## Heads or Tails
+
+# Instructions
+
+You are going to write a virtual coin toss program. It will randomly tell the user "Heads" or "Tails". 
+
+**Important**, the first letter should be capitalised and spelt exactly like in the example e.g. Heads, not heads.
+
+e.g.
+1 means Heads
+0 means Tails 
+
+# Example Output
+
+```
+Heads
+```
+
+or
+
+```
+Tails
+```
