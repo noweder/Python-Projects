5ea574510147b85f2acb82fee17242796b6319b3
noweder <hmnoweder@gmail.com>
2022-01-11 23:06:45 +0200

    updated code

--- Highest Score/highest-score.py
+++ Highest Score/highest-score.py
@@ -3,9 +3,9 @@ for n in range(0, len(student_scores)):
   student_scores[n] = int(student_scores[n])
 print(student_scores)
 
-max = 0
-for x in student_scores:
-    if x > max:
-        max = x
+highest_score = 0
+for score in student_scores:
+    if score > highest_score:
+        highest_score = score
 
-print(f"The highest score in the class is: {max}")
+print(f"The highest score in the class is: {highest_score}")
