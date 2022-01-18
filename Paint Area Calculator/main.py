72c64ef5a0cddc10612427bd1245ce8450142fdb
noweder <hmnoweder@gmail.com>
2022-01-18 20:56:53 +0200

    added area calc code

---
+++ Paint Area Calculator/main.py
@@ -0,0 +1,11 @@
+import math
+def paint_calc(height, width, cover):
+    total_cans = int(math.ceil((test_h * test_w) / cover))
+    print(f"You'll need {total_cans} cans of paint.")
+
+test_h = int(input("Height of wall: "))
+test_w = int(input("Width of wall: "))
+coverage = 5
+
+paint_calc(height=test_h, width=test_w, cover=coverage)
+
