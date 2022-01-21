47a210d7af3962b3acd656a55bf8fd7c0d38548e
noweder <hmnoweder@gmail.com>
2022-01-21 14:46:36 +0200

    modified code

--- Simple Grading Program/main.py
+++ Simple Grading Program/main.py
@@ -8,14 +8,14 @@ student_scores = {
 
 student_grades = {}
 
-for key in student_scores:
-    if 100 >= student_scores[key] >= 91:
-        student_grades[key] = "Outstanding"
-    elif 90 >= student_scores[key] >= 81:
-        student_grades[key] = "Exceeds Expectations"
-    elif 80 >= student_scores[key] >= 71:
-        student_grades[key] = "Acceptable"
+for student in student_scores:
+    if 100 >= student_scores[student] >= 91:
+        student_grades[student] = "Outstanding"
+    elif 90 >= student_scores[student] >= 81:
+        student_grades[student] = "Exceeds Expectations"
+    elif 80 >= student_scores[student] >= 71:
+        student_grades[student] = "Acceptable"
     else:
-        student_grades[key] = "Fail"
+        student_grades[student] = "Fail"
     
 print(student_grades)
